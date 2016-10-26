09a243426cf3b93693e469e4697cb65e3a3db1f8
WesStout <u1011688@utah.edu>
2016-10-26 00:08:03 -0600

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,70 +1,68 @@
 package assignment08;
 
+import static org.junit.Assert.assertFalse;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.NoSuchElementException;
+import assignment08.SortedSet;
 
-import assignment06.DoublyLinkedList.Node;
-import lab08.BinarySearchNode;
-
-public class BinarySearchTree<T extends Comparable<? super T>> implements SortedSet<T> {
+public class BinarySearchTree<T extends Comparable<? super T>> {
 
 	public Node root = null;
-	
+
 	private int size = 0;
-	
+
 	public BinarySearchTree() {
-		BinarySearchTree BST = new BinarySearchTree();
 		size = 0;
 	}
-	
+
 	/**
 	 * Node class that allows for .data, .prev. and .next
 	 *
 	 * @param <E>
 	 */
-    public class Node<T>{
-        private T data;
-        private Node left;
-        private Node right;
-        public Node(T data) {
-            this.data = data;
-        }
-        
-    }
-	
-    
-    public boolean addHelper(Node<T> currentNode, Node<T> toBeInput) {
-    	int compareValue = ((T)toBeInput.data).compareTo((T) currentNode.data);
-    	if (compareValue == 0) {
-    		return false;
-    	}
-    	if (compareValue < 0) {
-    		if (currentNode.left == null) {
-    			currentNode.left = toBeInput;
-    			size++;
-    			return true;
-    		}
+	public class Node<T> {
+		public T data;
+		private Node left;
+		private Node right;
+
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+
+	public boolean addHelper(Node<T> currentNode, Node<T> toBeInput) {
+		int compareValue = ((T) toBeInput.data).compareTo((T) currentNode.data);
+		if (compareValue == 0) {
+			return false;
+		}
+		if (compareValue < 0) {
+			if (currentNode.left == null) {
+				currentNode.left = toBeInput;
+				size++;
+				return true;
+			}
 			if (currentNode.left != null) {
 				addHelper(currentNode.left, toBeInput);
 			}
-    	}
-    	if (compareValue > 0) {
-    		if (currentNode.right == null) {
-    			currentNode.right = toBeInput;
-    			size++;
-    			return true;
-    		}
+		}
+		if (compareValue > 0) {
+			if (currentNode.right == null) {
+				currentNode.right = toBeInput;
+				size++;
+				return true;
+			}
 			if (currentNode.right != null) {
 				addHelper(currentNode.right, toBeInput);
 			}
-    	}
-    	return false;
-    }
-    
-    
-    
-	@Override
+		}
+		return false;
+	}
+
+	
 	public boolean add(Comparable item) {
 		if (item == null) {
 			return false;
@@ -75,55 +73,89 @@ public class BinarySearchTree<T extends Comparable<? super T>> implements Sorted
 			element.left = null;
 			element.right = null;
 			size = 1;
-		}
-		else {
+		} else {
 			if (addHelper(root, element) == false) {
 				return false;
-			}
-			else {
+			} else {
 				return true;
 			}
 		}
-		
 		return false;
 	}
 
-	@Override
-	public boolean addAll(Collection items) {
-	
+
+	public boolean addAll(Collection<? extends T> items) {
+		int count = 0;
+		if (items != null) {
+			for ( T item : items) {
+				add(item);
+				count++;
+			}
+			if (count != 0) {
+				return true;
+			}
+		}
 		return false;
+
 	}
 
-	@Override
+
 	public void clear() {
 		root = null;
 		size = 0;
 	}
 
-	@Override
+	public boolean containsHelper(Node<T> currentNode, Node<T> element) {
+		int compareValue = ((T) element.data).compareTo((T) currentNode.data);
+		if (compareValue == 0) {
+			return true;
+		}
+		if (compareValue < 0) {
+			if (currentNode.left != null) {
+				return containsHelper(currentNode.left, element);
+			}
+		}
+		if (compareValue > 0) {
+			if (currentNode.right != null) {
+				return containsHelper(currentNode.right, element);
+			}
+		}
+		return false;
+	}
+
 	public boolean contains(Comparable item) {
-		
+		if (item == null || size == 0) {
+			return false;
+		}
+		Node element = new Node(item);
+		if (containsHelper(root, element)) {
+			return true;
+		}
 		return false;
 	}
 
-	@Override
-	public boolean containsAll(Collection items) {
-		
-		return false;
+
+	public boolean containsAll(Collection<? extends T> items) {
+		if (items != null) {
+			for ( T item : items) {
+				if (contains(item) == false){
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
-	@Override
+
 	public T first() throws NoSuchElementException {
-		if (root == null) {
+		if (root != null) {
+			return (T)root.data;
+		} else {
 			throw new NoSuchElementException();
 		}
-		else {
-			Node<T> node =  getLeftMostNode(root);
-			return node.data;
-		}
 	}
 
-	@Override
+
 	public boolean isEmpty() {
 		if (size == 0) {
 			return true;
@@ -131,72 +163,141 @@ public class BinarySearchTree<T extends Comparable<? super T>> implements Sorted
 		return false;
 	}
 
-	@Override
+	
 	public T last() throws NoSuchElementException {
 		if (root == null) {
 			throw new NoSuchElementException();
+		} else {
+			return (T) getRightMostNode(root).data;
 		}
-		else {
-			Node<T> node =  getRightMostNode(root);
-			return node.data;
-		}
+		// return null;
 	}
-	
+
 	public Node<T> getLeftMostNode(Node<T> currentNode) {
 		if (currentNode.left == null) {
-			return currentNode.left;
+			return currentNode;
 		}
 		return getLeftMostNode(currentNode.left);
 	}
-	
+
 	public Node<T> getRightMostNode(Node<T> currentNode) {
 		if (currentNode.right == null) {
-			return currentNode.right;
+			return currentNode;
 		}
-		return getLeftMostNode(currentNode.right);
+		return getRightMostNode(currentNode.right);
+	}
+
+	public boolean removeHelper(Node<T> currentNode, Node<T> toBeRemoved) {
+		int compareValue = ((T) toBeRemoved.data).compareTo((T) currentNode.data);
+		if (root.data == toBeRemoved.data) {
+			if (currentNode.left != null && currentNode.right != null) {
+				Node<T> leftMostNodeRightSide = getLeftMostNode(currentNode.right);
+				currentNode.data = leftMostNodeRightSide.data;
+				if (leftMostNodeRightSide.right != null) {
+					currentNode.right.left = leftMostNodeRightSide.right;
+				}
+				else {
+					currentNode.right.left = null;
+				}
+				size--;
+				size = 0;
+				return true;
+			}
+			else if (currentNode.left == null && currentNode.right != null) {
+				root = root.right;
+				size--;
+				return true;
+			}
+			else if (currentNode.left != null && currentNode.right == null) {
+				root = root.left;
+				size--;
+				return true;
+			}
+			else if (currentNode.left == null && currentNode.right == null) {
+				size = 0;
+				root = null;
+				return true;
+			}
+		}
+		
+		if (compareValue < 0) {
+			if (currentNode.left != null) {
+				//if (currentNode.left.data == toBeRemoved.data) {
+					
+					if (currentNode.left.left != null & currentNode.left.right != null) {
+						Node<T> leftMostNodeRightSide = getLeftMostNode(currentNode.left.right);
+						if (leftMostNodeRightSide.right != null) {
+							if (currentNode.left.right.data == leftMostNodeRightSide.data) {
+								currentNode.left.data = currentNode.left.right.data;
+								currentNode.left.right = currentNode.left.right.right;
+							}	
+							else {
+								setLeftMostNodeRightKid(currentNode.left.right, leftMostNodeRightSide);
+							}
+						}
+						currentNode.left.data = leftMostNodeRightSide.data;
+					}
+					
+					else if (currentNode.left.left == null && currentNode.left.right != null) {
+						currentNode.left = currentNode.left.right;
+					}
+					else if (currentNode.left.left != null && currentNode.left.right == null) {
+						currentNode.left = currentNode.left.left;
+					}
+					else if (currentNode.left.left == null && currentNode.left.right == null) {
+						currentNode.left = null;
+					}
+					size--;
+				//}
+				
+			}
+		removeHelper(currentNode.left, toBeRemoved);	
+		}
+				
+		if (compareValue > 0) {
+			if (currentNode.right != null) {
+				if (currentNode.right.data == toBeRemoved.data) {
+					if (currentNode.right.left != null & currentNode.right.right != null) {
+						Node<T> leftMostNodeRightSide = getLeftMostNode(currentNode.right.right);
+						if (leftMostNodeRightSide.right != null) {
+							if (currentNode.left.right.data == leftMostNodeRightSide.data) {
+								currentNode.left.data = currentNode.left.right.data;
+								currentNode.left.right = currentNode.left.right.right;
+							}	
+							else {
+								setLeftMostNodeRightKid(currentNode.left.right, leftMostNodeRightSide);
+							}
+						}
+						currentNode.left.data = leftMostNodeRightSide.data;
+					}
+					else if (currentNode.right.left == null && currentNode.right.right != null) {
+						currentNode.right = currentNode.right.right;
+					}
+					else if (currentNode.right.left != null && currentNode.right.right == null) {
+						currentNode.right = currentNode.right.left;
+					}
+					else if (currentNode.right.left == null && currentNode.right.right == null) {
+						currentNode.right = null;
+					}
+					size--;
+				}
+				return true;
+			}
+		removeHelper(currentNode.right, toBeRemoved);	
+		}	
+		return false;
 	}
 	
-	public boolean removeHelper(Node<T> currentNode, Node<T> toBeRemoved) {
-    	int compareValue = ((T)toBeRemoved.data).compareTo((T) currentNode.data);
-    	if (compareValue == 0 && currentNode.left == null && currentNode.right == null) {
-    		currentNode = null;
-    		size--;
-    		return true;
-    	}
-    	if (compareValue == 0 && currentNode.right.left == null) {
-    		currentNode.right.left = currentNode.left;
-    		currentNode = currentNode.right;
-    		size--;
-    		return true;
-    	}
-    	if (compareValue == 0 && currentNode.right.left != null) {
-    		Node<T> leftMostNode = getLeftMostNode(currentNode.right);
-    		if (leftMostNode.right != null) {
-    			currentNode.data = leftMostNode.data;
-    			leftMostNode = leftMostNode.right;
-    		}
-    		else {
-    			currentNode.data = leftMostNode.data;
-    			leftMostNode = null;
-    		}
-    		size--;
-			return true;
-    	}
-    	
-    	if (compareValue < 0) {
-			if (currentNode.left != null) {
-				removeHelper(currentNode.left, toBeRemoved);
-			}
-    	}
-    	if (compareValue > 0) {
-			if (currentNode.right != null) {
-				removeHelper(currentNode.right, toBeRemoved);
-			}
-    	}
-    	return false;
-    }
+	public void setLeftMostNodeRightKid(Node<T> node, Node<T> leftMostNode) {
+		
+		if (node.left.data == leftMostNode.data) {
+			node.left = leftMostNode.right;
+		}
+		else {
+			 setLeftMostNodeRightKid(node.left, leftMostNode);
+		} 
+	}
 
-	@Override
 	public boolean remove(Comparable item) {
 		if (item == null || size == 0) {
 			return false;
@@ -206,25 +307,109 @@ public class BinarySearchTree<T extends Comparable<? super T>> implements Sorted
 			return false;
 		}
 		return true;
-
 	}
 
-	@Override
-	public boolean removeAll(Collection items) {
-		
-		return false;
+
+	public boolean removeAll(Collection<? extends T> items) {
+		int count = 0;
+		if (items != null) {
+			for ( T item : items) {
+				if (remove(item)){
+					count++;
+				}
+			}
+		}
+		if (count != 0){
+			return true;
+		}
+		return true;
 	}
 
-	@Override
+
 	public int size() {
 		return size;
 	}
 
-	@Override
+	
 	public ArrayList toArrayList() {
-		
-		return null;
+		ArrayList<T> array = new ArrayList<T>();
+		toArrayListHelper(root, array);
+		return array;
 	}
 
+	public void toArrayListHelper(Node node, ArrayList<T> array) {
+		if (node == null) {
+			return;
+		}
+		toArrayListHelper(node.left, array);
+		array.add((T) node.data);
+		toArrayListHelper(node.right, array);
+	}
+
+	public void writeDot(String filename) {
+		try {
+			// PrintWriter(FileWriter) will write output to a file
+			PrintWriter output = new PrintWriter(new FileWriter(filename));
+			// Set up the dot graph and properties
+			output.println("digraph BST {");
+			output.println("node [shape=record]");
+			if (root != null)
+				writeDotRecursive(root, output);
+			// Close the graph
+			output.println("}");
+			output.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Recursive method for writing the tree to a dot file
+	private void writeDotRecursive(Node n, PrintWriter output) throws Exception {
+		output.println(n.data + "[label=\"<L> |<D> " + n.data + "|<R> \"]");
+		if (n.left != null) {
+			// write the left subtree
+			writeDotRecursive(n.left, output);
+			// then make a link between n and the left subtree
+			output.println(n.data + ":L -> " + n.left.data + ":D");
+		}
+		if (n.right != null) {
+			// write the left subtree
+			writeDotRecursive(n.right, output);
+			// then make a link between n and the right subtree
+			output.println(n.data + ":R -> " + n.right.data + ":D");
+		}
+	}
 	
+	
+	public static void main(String[] args)  {
+		BinarySearchTree bst = new BinarySearchTree();
+		bst.add(500);
+		bst.add(900);
+		bst.add(1200);
+		bst.add(1050);
+		bst.add(1025);
+		bst.add(1075);
+		bst.add(1100);
+		bst.add(1125);
+		bst.add(1120);
+		bst.add(1130);
+		bst.add(1025);
+		bst.add(1000);
+		bst.add(975);
+		bst.add(980);
+		bst.add(250);
+		bst.add(200);
+		bst.add(400);
+		bst.add(375);
+		bst.add(350);
+		bst.add(380);
+		bst.add(377);
+		bst.add(100);
+		bst.add(10);
+		bst.add(90);
+		bst.add(50);
+		bst.add(70);
+		bst.remove(90);
+	}
+
 }
